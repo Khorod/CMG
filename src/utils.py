bf5b2371c339873f757a0c91ffd66c3e66b71a4a
Steven Laan <therealneojume@gmail.com>
2013-05-16 09:56:54 +0200

    Enhanced Point and fixed pathfinding

--- src/utils.py
+++ src/utils.py
@@ -7,27 +7,27 @@ class Point(tuple):
         super(Point, self).__init__(x,y)
 
     def __isub__(self, other):
-        if isinstance(other, Point):
+        if isinstance(other, Point) or isinstance(other, tuple):
             self = Point(self[0] - other[0], self[1] - other[1])
             return self
         else:
             raise NotImplementedError()
 
     def __sub__(self, other):
-        if isinstance(other, Point):
+        if isinstance(other, Point) or isinstance(other, tuple):
             return Point(self[0] - other[0], self[1] - other[1])
         else:
             raise NotImplementedError()
 
     def __iadd__(self, other):
-        if isinstance(other, Point):
+        if isinstance(other, Point) or isinstance(other, tuple):
             self = Point(self[0] + other[0], self[1] + other[1])
             return self
         else:
             raise NotImplementedError()
 
     def __add__(self, other):
-        if isinstance(other, Point):
+        if isinstance(other, Point) or isinstance(other, tuple):
             return Point(self[0] + other[0], self[1] + other[1])
         else:
             raise NotImplementedError()
@@ -45,15 +45,31 @@ class Point(tuple):
         else:
             raise NotImplementedError()
 
+    def __getattribute__(self, name):
+        if name == 'x':
+            return self[0]
+        elif name == 'y':
+            return self[1]
+        else:
+            return object.__getattribute__(self, name)
+
+    def __eq__(self, other):
+        if isinstance(other, Point) or isinstance(other, tuple):
+            return self[0] == other[0] and self[1] == other[1]
+        else:
+            raise NotImplementedError()
+
     def dist(self, other):
-        if isinstance(other, Point):
+        if isinstance(other, Point) or isinstance(other, tuple):
             diff = self - other
             return (diff[0]**2 + diff[1]**2)**0.5
         else:
             return NotImplementedError()
-
 if __name__ == '__main__':
     origin = Point(0,0)
     p = Point(3,4)
     p2 = Point(3,4)
+    print p + p2
+    print p + (1,2)
+    print 'x of p:', p.x
     print hash(p), hash(p2)
--- src/world.py
+++ src/world.py
@@ -1,29 +1,68 @@
 import astar
+from utils import Point
 
+COLLISION_MAP = [
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 1, 1, 1, 1, 1, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 class World:
-
     def __init__(self, collision_map, flow_map, mesh):
         self.collision_map = collision_map
         self.flow_map = flow_map
         self.mesh = mesh
+        self.height = 10
+        self.width  = 10
+        self.tilesize = 40
 
     def plan_path(self, start, goal):
-        path, _ = astar.astar(start, self.neighbors, goal, 0, 
-            self.cost, self.heuristic)
+        goal_func = lambda x: x == goal
+        heur_func = lambda x: goal.dist(x)
+        cost_func = lambda x,y: 1
+
+        path, _ = astar.astar(start, self.neighbors, goal_func, 0, 
+            cost_func, heur_func)
         return path
 
-    def neighbors(self, pos):
-        return self.mesh[pos]
+    def neighbors(self, pos, wall = 1):
+        if pos.x > 0:
+            if self.collision_map[pos.x - 1][pos.y] < wall:
+                yield pos - (1, 0)
+
+        if pos.x < self.width - 1:
+            if self.collision_map[pos.x + 1][pos.y] < wall:
+                yield pos + (1, 0)
+        
+        if pos.y > 0:
+            if self.collision_map[pos.x][pos.y - 1] < wall:
+                yield pos - (0, 1)
+
+        if pos.y < self.height - 1:
+            if self.collision_map[pos.x][pos.y + 1] < wall:
+                yield pos + (0, 1)
 
     def cost(self, start, end):
-        pass
+        return 1
 
     def heuristic(self, start, end):
-        pass
+        return start.dist(end)
 
     def __repr__(self):
         pass
 
     def __str__(self):
         pass
+
+if __name__ == '__main__':
+    world = World(COLLISION_MAP, None, None)
+    print world.plan_path(Point(1,1), Point(6,4))
+
