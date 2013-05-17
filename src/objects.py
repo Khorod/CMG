c1e6b02109c29f0d88968c697c50613e1f9721fb
Michael Cabot <michael.a.cabot@gmail.com>
2013-05-17 13:51:19 +0200

    color and radius are variables of class Person. Add class Cop. Player gets random default color

--- src/objects.py
+++ src/objects.py
@@ -1,4 +1,7 @@
+"""By Michael Cabot, Steven Laan, Richard Rozeboom"""
+
 import pygame
+from random import randint
 
 # Own imports
 import utils
@@ -12,16 +15,23 @@ class GameObject(object):
         raise NotImplementedError()
 
     def step(self):
-        """Executes the main logic of the object. This method is called every timestep."""
+        """Executes the main logic of the object. This method is called every 
+        timestep."""
         raise NotImplementedError()
 
 class Person(GameObject):
     """Class for one person."""
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, color = None, radius = 8):
         GameObject.__init__(self)
         self.pos = utils.Point(x, y)
         self.goal = None
+        if color != None:
+            self.color = color
+        else:
+            self.color = (randint(0, 255), randint(0, 255), randint(0, 255))
+
+        self.radius = radius
         
     def __repr__(self):
         return self.pos.__repr__()
@@ -30,16 +40,28 @@ class Person(GameObject):
         return self.__repr__()
 
     def draw(self, surface):
-        pygame.draw.circle(surface, (255,0,0), self.pos.tuple(), 8, 0)
+        pygame.draw.circle(surface, self.color, tuple(self.pos), self.radius, 0)
 
     def step(self):
         pass
-        
+
+
 class Player(Person):
     """Player object."""
 
     def __init__(self, x, y):
-        Person.__init__(self, x, y)
+        Person.__init__(self, x, y, (255, 0, 0))
+        
+    def step(self):
+        # Logic here!
+        pass
+
+
+class Cop(Person):
+    """Cop object."""
+
+    def __init__(self, x, y):
+        Person.__init__(self, x, y, (0, 0, 255))
         
     def step(self):
         # Logic here!
